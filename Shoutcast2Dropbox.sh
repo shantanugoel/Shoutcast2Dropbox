6a6ebc59dfc0d6f15434a80b7cd231525cf04e9b
Shantanu Goel <shantanu.goel@gmail.com>
2015-02-15 00:53:33 +0530

    Initial working version

--- Shoutcast2Dropbox.sh
+++ Shoutcast2Dropbox.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 VERSION=0.1
+#Path to dropbox uploader script. Change this to the correct/absolute path according to how you installed Shoutcast2Dropbox and Dropbox Uploader
+DROPBOX_UPLOADER="./Dropbox-Uploader/dropbox_uploader.sh"
 
 #Usage
 if [ "$#" != 3 ]; then
-  echo "Shoutcast2Dropbox Playlist Maker v$VERSION - Shantanu Goel"
-  echo ""
-  echo "Usage: $0 <Input File for station lists> <Cumulative Duration to record in seconds> <Output Directory path>"
-  echo "Input file should have 1 station url per line"
+  echo "Shoutcast2Dropbox Playlist Maker v$VERSION - Shantanu Goel\n"
+  echo "https://github.com/shantanugoel/Shoutcast2Dropbox\n"
+  echo "Usage: $0 <Input File for station lists> <Cumulative Duration to record in seconds> <Output Directory path>\n"
+  echo "Input file should have 1 station url per line\n"
   exit 1
 fi
 
@@ -23,6 +25,8 @@ duration_per_station=$(($total_duration_in_seconds / $num_stations))
 
 #Delete existing songs from last run
 #Still playing a little safe and deleting mp3/aac files instead of everything. If your station broadcasts in another format, add that here
+$DROPBOX_UPLOADER -q delete $output_dir/*.aac
+$DROPBOX_UPLOADER -q delete $output_dir/*.mp3
 rm -f $output_dir/*.aac
 rm -f $output_dir/*.mp3
 
@@ -42,3 +46,4 @@ rm -rf $output_dir/incomplete
 
 #Upload the new songs
 ls $output_dir
+$DROPBOX_UPLOADER -q -s upload $output_dir `basename $output_dir`
